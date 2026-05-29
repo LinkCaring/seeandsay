c018775d4759fbfb831b11e8a3db5e9bb217f1fc
linkcaring2-ops <linkcaring2@gmail.com>
2026-05-29 22:37:47 +0300

    5.5 footer

--- frontend_demo/js/app/app.js
+++ frontend_demo/js/app/app.js
@@ -368,7 +368,7 @@ function App() {
     React.createElement(
       "div",
       { className: "app-version-label", "aria-hidden": "true" },
-      "version 5.4"
+      "version 5.5"
     ),
     /* Reset button moved to test navbar */
     showResetConfirm
